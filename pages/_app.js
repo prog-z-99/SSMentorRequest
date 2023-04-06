106fd149a3e4f3163c4f3430f4801b16edff4b72
Z <programming.zolboo@gmail.com>
2023-04-06 14:22:33 +0800

    changed to dark mode. layout shift fixed

--- pages/_app.js
+++ pages/_app.js
@@ -4,15 +4,20 @@ import React from "react";
 import "./styles.css";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
-import { createEmotionCache } from "@mantine/core";
+// import { createEmotionCache } from "@mantine/core";
 dayjs.extend(localizedFormat);
 
-const myCache = createEmotionCache({ key: "mantine" });
+// const myCache = createEmotionCache({ key: "mantine" });
 // Use the <Provider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
 export default function App({ Component, pageProps }) {
   return (
-    <MantineProvider withNormalizeCSS withGlobalStyles emotionCache={myCache}>
+    <MantineProvider
+      withNormalizeCSS
+      withGlobalStyles
+      // emotionCache={myCache}
+      theme={{ colorScheme: "dark" }}
+    >
       <SessionProvider refetchInterval={5 * 60} session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
