2b8e8bbe6893481f1a3450c3fc5aebb283c45fe4
Fish <uselessfishlol@gmail.com>
2024-05-09 21:24:22 -0700

    add mentor guide button

--- pages/mentors/mentorguide.jsx
+++ pages/mentors/mentorguide.jsx
@@ -1,36 +1,45 @@
+import { Button, Flex } from '@mantine/core'
+import { useRouter } from 'next/router'
 import MdxLayout from '../../components/mdx-layout'
 import MentorGuide from '../../components/Mentorguide.mdx'
 
-const CustomH1 = ({ children }) => (
-  <h1 style={{ fontSize: '3em', margin: 0 }}>{children}</h1>
-)
-
-const CustomH2 = ({ children }) => (
-  <h2 style={{ margin: '1em 0 0 0' }}>{children}</h2>
-)
-
-const CustomH3 = ({ children }) => (
-  <h3 style={{ margin: '0.5em 0' }}>{children}</h3>
-)
-
-const CustomH4 = ({ children }) => (
-  <h4 style={{ fontSize: '1.1em', margin: '1.7em 0 0' }}>{children}</h4>
-)
-
-const CustomP = ({ children }) => (
-  <p style={{ margin: '1em 0' }}>{children}</p>
-)
-
-const overrideComponents = {
-  h1: CustomH1,
-  h2: CustomH2,
-  h3: CustomH3,
-  h4: CustomH4,
-  p: CustomP
-}
-
 export default function MentorGuidePage() {
-  return <MdxLayout>
-    <MentorGuide components={overrideComponents} />
-  </MdxLayout>
+  const CustomH1 = ({ children }) => (
+    <h1 style={{ fontSize: '3em', margin: 0 }}>{children}</h1>
+  )
+
+  const CustomH2 = ({ children }) => (
+    <h2 style={{ margin: '1em 0 0 0' }}>{children}</h2>
+  )
+
+  const CustomH3 = ({ children }) => (
+    <h3 style={{ margin: '0.5em 0' }}>{children}</h3>
+  )
+
+  const CustomH4 = ({ children }) => (
+    <h4 style={{ fontSize: '1.1em', margin: '1.7em 0 0' }}>{children}</h4>
+  )
+
+  const CustomP = ({ children }) => (
+    <p style={{ margin: '1em 0' }}>{children}</p>
+  )
+
+  const overrideComponents = {
+    h1: CustomH1,
+    h2: CustomH2,
+    h3: CustomH3,
+    h4: CustomH4,
+    p: CustomP
+  }
+
+  const router = useRouter();
+
+  return (
+    <MdxLayout>
+      <MentorGuide components={overrideComponents} />
+      <Flex justify={'flex-end'}>
+        <Button onClick={() => router.push('/mentors/requests')}>Let's Go!</Button>
+      </Flex>
+    </MdxLayout>
+  )
 }
--- pages/mentors/requests.jsx
+++ pages/mentors/requests.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Button, Flex } from "@mantine/core";
+import Link from "next/link";
 import Layout from "../../components/layout";
-
+import { useRouter } from "next/router";
 import useAuthTest from "../../hooks/useAuthTest";
 import { MentorRequestComponent } from "../../components/MentorRequestComponents";
 
 export default function MentorsRequests() {
+  const router = useRouter();
   const { allRequests, isAdmin, loading, notAuth } = useAuthTest(
     "/api/admin/requests",
     true
@@ -12,6 +15,15 @@ export default function MentorsRequests() {
 
   return (
     <Layout loading={loading} notAuth={notAuth}>
+      <Flex justify={'flex-end'}>
+        <Button
+          variant="gradient"
+          gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+          onClick={() => router.push('/mentors/mentorguide')}
+          >
+          Mentor Guide
+        </Button>
+      </Flex>
       <MentorRequestComponent allRequests={allRequests} isAdmin={isAdmin} />
     </Layout>
   );
