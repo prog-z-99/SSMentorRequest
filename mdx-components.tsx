04c0d1b682d65a1accd579e4d584fab01b08d310
Fish <uselessfishlol@gmail.com>
2024-05-09 20:49:53 -0700

    add custom styles to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,13 +2,6 @@ import type { MDXComponents } from 'mdx/types'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h3: ({ children }) => (
-      <h3 style={{ margin: '0.8rem'}}>{children}</h3>
-    ),
-    h4: ({ children }) => (
-      <h4 style={{ fontSize: '1.17em'}}>{children}</h4>
-    ),
-    
     ...components,
   }
 }
